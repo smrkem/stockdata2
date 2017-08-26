5c31fd1bdbc3d1e8efbbc9836e3fe4e69cef2cce
matt <smrkem@gmail.com>
2017-08-25 23:29:10 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
+const srcDir = path.resolve(__dirname, 'src')
+
+
+module.exports = {
+  entry: `${srcDir}/index.js`,
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.js$/,
+      loader: 'standard-loader',
+      exclude: /node_modules/
+    },
+    {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${srcDir}/index.html`
+    }),
+    new DashboardPlugin()
+  ]
+}
