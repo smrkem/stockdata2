1b4976241c1e0f178fcc4bfd9186aa15f6092b1a
matt <smrkem@gmail.com>
2017-09-07 21:09:38 -0400

    initial reading, saving data to s3

--- lambdas/store_news_items/src/lambda_function.py
+++ lambdas/store_news_items/src/lambda_function.py
@@ -1,15 +1,38 @@
 import json
-
+import boto3
 
 def lambda_handler(event, context):
-    # Get posted data from fe app
-    print("BODY: {}".format(event['body']))
-    print("RQCntxt: {}".format(event['requestContext']))
-    # Store each in s3 bucket
-    # Update the visited_sites index file in s3
+    s3 = boto3.resource('s3')
 
+    obj = s3.Bucket('ms-stocknewsitems').Object('positive.json')
+    body = obj.get()['Body'].read()
 
-    output = { 'message': "hello karl..."}
+    pos_posts = json.loads(body)
+
+    # pos_posts = [
+    #     {
+    #         'title': "Sample Title 1",
+    #         'url': "http://example.com",
+    #         'category': 'pos',
+    #         'body': "The body of the post goes in here, all paragraphs concatenated into a single string."
+    #     }
+    # ]
+    #
+    # s3.Bucket('ms-stocknewsitems').put_object(Key='positive.json', Body=json.dumps(pos_posts))
+    #
+
+    # # Get posted data from fe app
+    # print("BODY: {}".format(event['body']))
+    # posts = json.loads(event['body'])
+    # # Store each in s3 bucket
+    # # Update the visited_sites index file in s3
+    #
+
+    output = {
+        'message': "Get contents from s3",
+        'posts': posts
+    }
+
     return {
         'statusCode': 200,
         'headers': {
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 StockNewsData P.1: Scraping For Data. A static hosted React app with a Serverless backend
+
+
+my bucket arn:  
+- arn:aws:s3:::ms-stocknewsitems
