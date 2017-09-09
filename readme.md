b63e7113b3f1fae135c3dc33db705748fbea790f
matt <smrkem@gmail.com>
2017-09-09 11:13:24 -0400

    empty dir

--- readme.md
+++
@@ -1,5 +0,0 @@
-StockNewsData P.1: Scraping For Data. A static hosted React app with a Serverless backend
-
-
-my bucket arn:  
-- arn:aws:s3:::ms-stocknewsitems
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-
-import 'normalize.css'
-import './base.css'
-import App from './components/App/App'
-
-render(<App />, document.getElementById('app'))
