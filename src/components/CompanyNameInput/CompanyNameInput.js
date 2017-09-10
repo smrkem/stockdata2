fe3e58e0a8599c3ba90dbc3e11305633f428fc74
matt <smrkem@gmail.com>
2017-09-10 17:27:33 -0400

    upgrades CompanyNameInput

--- src/components/CompanyNameInput/CompanyNameInput.js
+++ src/components/CompanyNameInput/CompanyNameInput.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import './CompanyNameInput.css'
 
-const CompanyNameInput = () => (
-  <div id="company-name-input">
-    <p>Enter the company name to search for:</p>
-    <form onSubmit={(e) => {
-        e.preventDefault()
-        console.log('submitted')
-      }}>
-      <p>
-        <input type="text"></input>
-        <button>GO</button>
-      </p>
-    </form>
-  </div>
-)
+class CompanyNameInput extends React.Component {
+
+  onFormSubmit(e) {
+    e.preventDefault()
+    let input = e.target.elements[0]
+    console.log(`Doing something with: ${input.value}`)
+    // Do something ...
+    input.value = null
+  }
+
+  render() {
+    return (
+     <div id="company-name-input">
+       <p>Enter the company name to search for:</p>
+       <form
+         onSubmit={(e) => {this.onFormSubmit(e)}}
+       >
+         <p>
+           <input type="text"></input>
+           <button>GO</button>
+         </p>
+       </form>
+     </div>
+   )
+  }
+}
+
 export default CompanyNameInput
