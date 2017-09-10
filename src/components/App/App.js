d5acf6670aea8d12c5346d7641fcf6ce980e8d96
matt <smrkem@gmail.com>
2017-09-10 15:10:03 -0400

    adds header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import './App.css'
+import Header from '../Header/Header'
 
 const App = () => (
   <div className="container">
+    <Header />
     <h1>Hello karl...</h1>
   </div>
 )
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './header.css'
+
+const Header = () => (
+  <header>
+    <div className="container header-container">
+      <h1>Stock News Scraper / Classifier</h1>
+      <nav>
+        <ul>
+        </ul>
+      </nav>
+    </div>
+  </header>
+)
+
+export default Header
