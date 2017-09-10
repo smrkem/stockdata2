04c7134e346dd03477ee9e3341e7e8cbbfd2ce10
matt <smrkem@gmail.com>
2017-09-10 17:01:06 -0400

    checks form submit

--- src/components/CompanyNameInput/CompanyNameInput.js
+++ src/components/CompanyNameInput/CompanyNameInput.js
@@ -4,10 +4,15 @@ import './CompanyNameInput.css'
 const CompanyNameInput = () => (
   <div id="company-name-input">
     <p>Enter the company name to search for:</p>
-    <p>
-      <input type="text"></input>
-      <button>GO</button>
-    </p>
+    <form onSubmit={(e) => {
+        e.preventDefault()
+        console.log('submitted')
+      }}>
+      <p>
+        <input type="text"></input>
+        <button>GO</button>
+      </p>
+    </form>
   </div>
 )
 export default CompanyNameInput
