a890f30d11232e498b3c71d20cdee1a69c8659df
matt <smrkem@gmail.com>
2017-09-16 13:10:24 -0400

    postItem contents

--- src/components/PostItem/PostItem.js
+++ src/components/PostItem/PostItem.js
@@ -14,6 +14,26 @@ const PostItem = (props) => {
           <a href={props.link} target='_blank'>{ props.link }</a>
         </span>
       </p>
+
+      <div className={ `item-cat cat-${props.category}` }>
+        <div className="cat-label">{ props.category }</div>
+        <div className="cat-actions">
+          <button className="spam-button" >
+              SPAM
+          </button>
+          <button className="good-button" >
+              GOOD
+          </button>
+          <button className="trash-button" >
+              DISCARD
+          </button>
+        </div>
+      </div>
+
+      <div className={(props.category != 'uncategorized' ? 'collapsed ':'') + "post-contents"}>
+        <h4 className="post-title">{ props.title }</h4>
+        { props.contents.map((p,i) => <p key={i}>{p}</p>) }
+      </div>
     </div>
   )
 }
